606bc6372486bb412cd3450f45a9c079d105595d
Bruno Ferraz <brunohenriqueferraz94@gmail.com>
2024-09-09 08:56:25 -0300

    adicionando biblioteca para tratar JSON

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatch;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import br.com.alura.service.ConsumoApi;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -13,6 +14,10 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Spring sem Web");
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=3cc7e0de");
+		System.out.println(json);
+		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
 	}
 }
